88eeab4b1e6b99d465a73016a0969971adab75a0
jamesbytes01-ops <jamesbytes01-ops@users.noreply.github.com>
2026-08-14 23:01:42 -0700

    Clean next.config.mjs for Cloudflare build

--- next.config.mjs
+++ next.config.mjs
@@ -4,20 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config, { isServer }) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: [
-        '**/node_modules/**',
-        '**/.git/**',
-        'C:/pagefile.sys',
-        'C:/hiberfil.sys',
-        'C:/swapfile.sys',
-        '**/pagefile.sys',
-      ],
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
